47062ace81e7be3d1844b1d710e1ccdbfd8b086f
guol1nag <guolianghe1996@gmail.com>
2020-03-19 19:25:10 +0800

    auto xgboost

---
+++ code/GBM_Feature_Eng/auto_feature_selection.py
@@ -0,0 +1,107 @@
+import copy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.model_selection as sk_ModelSelection
+import seaborn as sns
+import os
+import sklearn
+import random
+import xgboost
+
+
+class xgboost_utility:
+
+    def __init__(self, X_train, y_train, X_test, y_test, feature_map, **kwargs):
+        '''
+        param_grid sets hyper-parameter for the model
+
+        grid sets other parameter
+        '''
+        self.other_param = {
+
+        }
+
+        self.model_grid = {
+            # Number of gradient boosted trees. Equivalent to number of boosting rounds.
+            'num_estimators': kwargs['num_estimators'],
+
+            # Maximum tree depth for base learners.
+            'max_depth': kwargs['max_depth'],
+
+            # Boosting learning rate (xgb’s “eta”)
+            'learning_rate': kwargs['learning_rate'],
+
+            # 0 (silent), 1 (warning), 2 (info), 3 (debug).
+            'verbosity': kwargs['verbosity'],
+
+            'objective': kwargs['objective'],  # regression with squared loss
+            'min_child_weight': kwargs['min_child_weight'],
+
+            # L2 norm regularization, dafault 1
+            'lambda': kwargs['lambda'],
+            # 'subsample': 0.5,                 # Subsample ratio of the training instance
+            # 'colsample_bytree': 0.6,
+            # 'reg_lambda':,                   # L2 regularization term on weights
+        }
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.feature_map = feature_map
+
+        # instantiate model according to hyper-parameters
+        self.model = xgboost.XGBRegressor(**self.model_grid)
+
+    def training(self):
+        self.model.fit(self.X_train, self.y_train,
+                       eval_set=[(self.X_train, self.y_train),
+                                 (self.X_test, self.y_test)],
+                       eval_metric='rmse',
+                       verbose=True)
+
+    def feature_scores(self, feature_map):
+        raw_ranking = sorted(self.model.get_booster().get_score(
+        ).items(), key=lambda x: x[1], reverse=True)
+        ranking = []
+        for t in raw_ranking:
+            for key in self.feature_map:
+                if t[0] == key:
+                    ranking.append((self.feature_map[key], t[1]))
+        self.ranking = ranking
+        print(ranking)
+
+    def feature_selection(self, k):
+        '''
+        Args:
+            top k features that you want to preserve
+
+        Outputs:
+            return X_train, X_test with selected features
+        '''
+
+        return self.X_train, self.X_test
+
+    @staticmethod
+    def show_param():
+        print('''
+        Instantiate
+
+        Pass a dict contains all parameters, example is given in the following:
+
+        all_param={
+            ##### model parameters:
+
+            'num_estimators': 1000,    #  Number of gradient boosted trees. Equivalent to number of boosting rounds.
+            'max_depth': 10,            #  Maximum tree depth for base learners.
+            'learning_rate': 0.3,       #  Boosting learning rate (xgb’s “eta”)
+            'verbosity':1,               #  0 (silent), 1 (warning), 2 (info), 3 (debug).
+            'objective': 'reg:squarederror', # regression with squared loss
+            'min_child_weight': 10,  
+            'lambda':1                      # L2 norm regularization, dafault 1
+
+            ##### others:
+             
+            }
+
+        ''')
---
+++ code/GBM_Feature_Eng/xgboost_dataset.py
@@ -0,0 +1,125 @@
+import time
+import copy
+import numpy as np
+import pandas as pd
+import sklearn.model_selection as sk_ModelSelection
+import seaborn as sns
+import os
+import sklearn
+import random
+
+
+class xgboost_dataset():
+    def __init__(self, X_train, y_train, X_test, y_test):
+        '''
+        This class format data for xgboost
+        Args:
+            input type = np.darray or pandas
+
+            X_train: features, excluding return, [N_sample,N_feature]
+            y_train: return, [N_sample,]
+        '''
+        if type(X_train) is pd.DataFrame:
+            self.X_train = X_train.values
+            self.fea2num = {f'f{key}': X_train.columns[key]
+                            for key in range(len(X_train.columns))}
+        else:
+            self.X_train = X_train
+
+        if type(y_train) is pd.Series:
+            self.y_train = y_train.values
+        else:
+            self.y_train = y_train
+
+        self.full_data_train = np.hstack(
+            [self.X_train, self.y_train.reshape(-1, 1)])
+
+        if type(X_test) is pd.DataFrame:
+            self.X_test = X_test.values
+
+        else:
+            self.X_test = X_test
+
+        if type(y_test) is pd.Series:
+            self.y_test = y_test.values
+        else:
+            self.y_test = y_test
+
+        self.full_data_test = np.hstack(
+            [self.X_test, self.y_test.reshape(-1, 1)])
+
+    def split_dataset(self,
+                      encode_len: int,
+                      pred_len: int,):
+        self.X_train, self.y_train = self._xgb_walk_forward_split(
+            self.X_train, self.y_train, self.full_data_train, encode_len, pred_len, print_info=True)
+
+        self.X_test, self.y_test = self._xgb_walk_forward_split(
+            self.X_test, self.y_test, self.full_data_test, encode_len, pred_len, print_info=False)
+
+    def _xgb_walk_forward_split(self,
+                                x,
+                                y,
+                                full_data,
+                                encode_len: int,
+                                pred_len: int,
+                                print_info):
+        '''
+        see desirable split: https://github.com/guol1nag/datagrasp/blob/master/README.md
+
+        However, remember in XGBoost, data can only be formatted as X_train: [N_samples,N_features]
+                                                                    y: [N_sampples,]
+        It is therefore, we cannot make a batch, we can only use data in time t to predict time t+1
+
+        Args:
+
+            output -> np.array
+                self.x [N_samples,N_features + return]
+                self.y [N_samples,]
+        '''
+        N_samples = x.shape[0]
+
+        for i in range(0, N_samples, pred_len):
+            encode = full_data[i:min(
+                i+encode_len, N_samples)]
+
+            pred = y[i+encode_len:min(
+                i+encode_len+pred_len, N_samples)]
+            if pred.shape[0] != pred_len:   # to protect the prediction_length
+                break
+
+            if i == 0:
+                new_x = encode.reshape(1, -1)
+                new_y = pred.reshape(1, -1)
+            else:
+                try:
+                    new_x = np.concatenate(
+                        [new_x, encode.reshape(1, -1)], axis=0)
+                    new_y = np.concatenate(
+                        [new_y, pred.reshape(1, -1)], axis=0)
+                except:  # residual
+                    pass
+        if print_info:
+            print('number of samples:', new_x.shape[0])
+        return new_x, new_y
+
+    def batcher(self, x, y, batch_size):
+        '''
+        this method is deprecated
+
+        Args:
+            -> output results attributes:
+                x  [batch_size, encode_len, N_feature]
+                y  [batch_size, encode_len+pred_len]
+        '''
+        l = len(x)
+        for batch in range(0, l, batch_size):
+            yield (x[batch:min(batch + batch_size, l)], y[batch:min(batch + batch_size, l)])
+
+
+# if __name__ == "__main__":
+#     x = np.random.rand(20, 4)
+#     y = np.random.rand(20)
+#     xgb_data = xgboost_dataset(x, y)
+#     xgb_data.xgb_walk_forward_split(1, 1)
+#     print(xgb_data.x.shape)
