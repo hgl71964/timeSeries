0e5829a4d25fd190cb1d8c8f893ebbffd8806d1c
Chris Chia <chrischia06@gmail.com>
2020-03-19 21:58:03 +0000

    adding script to pull binance data

---
+++ code/assetdownloader.py
@@ -0,0 +1,58 @@
+import numpy as np
+import json
+import pandas as pd
+from binance.client import Client
+from datetime import datetime
+
+
+
+def downloadData(ticker, API_KEY, API_SECRET, FREQ, fullpath):
+	'''
+	Inputs: Binance API_KEY and API_SECRET, Binance symbol ticker, interval (Client.KLINE_INTERVAL_1HOUR, Client.KLINE_INTERVAL_1MINUTE)
+	Outputs: Writes to local
+	'''
+	keys = {"HOUR":Client.KLINE_INTERVAL_1HOUR, "MINUTE":Client.KLINE_INTERVAL_1MINUTE,"DAY":Client.KLINE_INTERVAL_1DAY}
+	binance_interval = keys[FREQ]
+	client = Client(API_KEY, API_SECRET)
+	klines = client.get_historical_klines(ticker+"USDT", binance_interval, "1 Jan, 2015")
+	# with open(f"{ticker}_{FREQ}.json", 'w') as fp:
+	#     json.dump(klines, fp)
+
+	#from binance api docs
+	columns = ["open_time","open","high","low","close","volume","close_time", "quote_asset_volume", "number_of_trades", " taker_buy_base_asset_volume", "taker_buy_quote_asset_volume","ignore"]
+	df = pd.DataFrame(klines)
+	df.columns=columns
+
+	#times are in milliseconds
+	df['open_time'] = df['open_time'].apply(lambda x: datetime.fromtimestamp(x/1000))
+	df['close_time'] = df['close_time'].apply(lambda x: datetime.fromtimestamp(x/1000))
+	path = f"{fullpath}/{ticker}USDT_{FREQ}.csv"
+	df.to_csv(path,index=False)
+	return df
+
+#add extra preprocessing here
+def transform(df):
+	df2 = df.copy()
+	df2['ohlc'] = np.mean(df2[['open', 'high', 'low','close']].values,axis=1)
+	df2['returns'] = (df2['close'] / df2['close'].shift(1))
+	df2['date'] = pd.to_datetime(df2['open_time']).dt.date
+	return df2
+
+def downloadWrapper(tickers, API_SECRET, API_KEY, FREQ, fullpath):
+  prices = {}
+  for x in tickers:
+      downloadData(x, API_KEY, API_SECRET, FREQ, fullpath) #change to HOUR, MINUTE if you want
+      prices[x] = transform(pd.read_csv(f"{fullpath}/{x}USDT_{FREQ}.csv"))
+
+  #Columns to take; drop open_time, close_time
+  cols = ['open', 'high', 'low', 'close', 'volume','quote_asset_volume', 'number_of_trades',
+        ' taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume','ohlc', 'returns', 'date']
+  temp = prices[tickers[0]][['date']+cols]
+  temp.columns = ['date']+[tickers[0]+"_"+ col for col in cols]
+  #Merge all price datas
+  for x in tickers[1:]:
+    temp2 = pd.DataFrame(prices[x][['date']+cols])
+    temp2.columns = ['date']+[x +"_" + col for col in cols]
+    temp = pd.merge(temp, temp2,how='left',on='date')
+  temp['date'] = pd.to_datetime(temp['date'])
+  return temp
