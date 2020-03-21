398cfdd8e63f0f4538095073b3556a49483abc9f
Chris Chia <chrischia06@gmail.com>
2020-03-21 20:30:32 +0000

    quick fix

--- code/indicator_downloader/merge.py
+++ code/indicator_downloader/merge.py
@@ -29,7 +29,7 @@ def merge(quandl_api_key, GLASSNODE_API_KEY, fullpath):
   sp500_data = sp500(fullpath)
   print("SP500 - COMPLETE")
   merged_feats['Date'] = pd.to_datetime(merged_feats['Date'])
-  sp500_data['Date'] = pd.to_datetime(sp500_data)['Date']
+  sp500_data['Date'] = pd.to_datetime(sp500_data['Date'])
   merged_feats = pd.merge(merged_feats,sp500_data, how='left',on='Date')
   glassnode = glassnodeDownloader(GLASSNODE_API_KEY, fullpath)
   glassnode['Date'] = pd.to_datetime(glassnode['Date'])
