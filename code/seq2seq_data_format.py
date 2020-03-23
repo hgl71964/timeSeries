c1b4ba907332969f127ed7424def13a4478a16cc
guol1nag <guolianghe1996@gmail.com>
2020-03-23 14:33:57 +0800

    fix

--- code/seq2seq_data_format.py
+++ code/seq2seq_data_format.py
@@ -1,21 +1,25 @@
 
 import torch
+import copy
 
 '''
 to format the dataset such that seq2seq can run
 '''
 
 
-class seq2seq_data_format():
-    def __init__(self, X, y):
+class seq2seq_dataset():
+    def __init__(self, X_train, y_train, X_test, y_test):
         """
         Args:
-            torch (Tensor): Dataset stored in Tensor
-            X: [N_samples,N-features] -> Tensor
-            y: [N_samples]  labels -> Tensor
+            X_train: [N_samples,N-features] -> Tensor
+            X_test: [N_samples,N-features] -> Tensor
+            y_train: [N_samples]  labels -> Tensor
+            y_test: [N_samples]  labels -> Tensor
         """
-        self._X = X  # feature
-        self._y = y  # price
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
 
     def walk_forward_split(self,
                            encode_len: int,
@@ -34,14 +38,15 @@ class seq2seq_data_format():
         '''
 
         # [N_samples,N-features + return]
-        self.full_data = torch.cat([self._X, self._y.unsqueeze(1)], dim=1)
+        self.full_data = torch.cat(
+            [self.X_train, self.y_train.unsqueeze(1)], dim=1)
 
-        N_samples = self._X.shape[0]
+        N_samples = self.X_train.shape[0]
         for i in range(0, N_samples, pred_len):
             encode = self.full_data[i:min(
                 i+encode_len, N_samples)]
 
-            pred = self._y[i+encode_len:min(
+            pred = self.y_train[i+encode_len:min(
                 i+encode_len+pred_len, N_samples)]
             if pred.shape[0] != pred_len:
                 break
@@ -57,7 +62,39 @@ class seq2seq_data_format():
                     # print(pred.unsqueeze(0).size())
                 except:  # residual
                     pass
-        print('maximum batch_size:', self.X.shape[0])
+        self.X_train = copy.deepcopy(self.X)
+        self.y_train = copy.deepcopy(self.y)
+        del self.X, self.y, self.full_data
+
+        # [N_samples,N-features + return]
+        self.full_data = torch.cat(
+            [self.X_test, self.y_test.unsqueeze(1)], dim=1)
+
+        N_samples = self.X_test.shape[0]
+        for i in range(0, N_samples, pred_len):
+            encode = self.full_data[i:min(
+                i+encode_len, N_samples)]
+
+            pred = self.y_test[i+encode_len:min(
+                i+encode_len+pred_len, N_samples)]
+            if pred.shape[0] != pred_len:
+                break
+
+            if i == 0:
+                self.X = encode.unsqueeze(0)
+                self.y = pred.unsqueeze(0)
+            else:
+                try:
+                    self.X = torch.cat([self.X, encode.unsqueeze(0)], dim=0)
+                    self.y = torch.cat([self.y, pred.unsqueeze(0)], dim=0)
+                    # print(encode.unsqueeze(0).size())
+                    # print(pred.unsqueeze(0).size())
+                except:  # residual
+                    pass
+
+        self.X_test = copy.deepcopy(self.X)
+        self.y_test = copy.deepcopy(self.y)
+        del self.X, self.y, self.full_data
 
     def bag_of_timeSeries_chunk_for_prediction(self,
                                                encode_len: int,
@@ -71,7 +108,7 @@ class seq2seq_data_format():
               X: [N_samples,N-features] Tensor N_samples have sequential properties
               y: [N_samples,]  labels Tensor    N_samples have sequential properties
 
-          output -> class attributes:
+        Returns
               self.X  [N_sample, encode_len, N_feature] N_feature excludes price
               self.y  [N_sample, pred_len]
         '''
@@ -98,13 +135,11 @@ class seq2seq_data_format():
                 except:
                     pass
                     # self._residual = segment.unsqueeze(0)
-        if shuffle:
-            self.shuffler()
 
-    def shuffler(self):
-        n = self._segment.size(0)
+    def shuffler(self, tensor):
+        n = tensor.size(0)
         rand = torch.randperm(n)
-        self._segment = self._segment[rand]
+        tensor = tensor[rand]
 
     def batcher(self, batch_size):
         '''
