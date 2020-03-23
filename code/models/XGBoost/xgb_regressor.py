b7329d3202cc7a3b56bf2eae6ec0a45ee9da664a
guol1nag <guolianghe1996@gmail.com>
2020-03-23 21:32:56 +0800

    fix

--- code/models/XGBoost/xgb_regressor.py
+++ code/models/XGBoost/xgb_regressor.py
@@ -93,14 +93,14 @@ class xgboost_utility:
         '''
         raw_ranking = sorted(self.model.get_booster().get_score(
         ).items(), key=lambda x: x[1], reverse=True)
-        ranking = []
-        for t in raw_ranking:
-            for key in self.feature_map:
-                if t[0] == key:
-                    ranking.append((self.feature_map[key], t[1]))
-        self.ranking = ranking
+        # ranking = []
+        # for t in raw_ranking:
+        #     for key in self.feature_map:
+        #         if t[0] == key:
+        #             ranking.append((self.feature_map[key], t[1]))
+        self.ranking = raw_ranking
 
-        return ranking
+        return raw_ranking
 
     def feature_selection(self, k: int):
         '''
