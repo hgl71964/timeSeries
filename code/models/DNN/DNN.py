4b07faa67d621d1aa33cbd0bf1171c8057392484
guol1nag <guolianghe1996@gmail.com>
2020-03-23 19:02:06 +0800

    fix

--- code/models/DNN/DNN.py
+++ code/models/DNN/DNN.py
@@ -76,9 +76,9 @@ class DNN_utility:
             if valid_loss < best_valid_loss:
                 best_valid_loss = valid_loss
                 self.best_model = copy.deepcopy(self.model)
-            print(f'Epoch: {epoch+1}:')
-            print(f'Train Loss: {train_loss:.3f}')
-            print(f'Validation Loss: {valid_loss:.3f}')
+                print(f'Epoch: {epoch+1}:')
+                print(f'Train Loss: {train_loss:.3f}')
+                print(f'Validation Loss: {valid_loss:.3f}')
 
         return best_valid_loss
 
