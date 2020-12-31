6ae3d2d9c7a6f1e19800ee1386a081f9cbf891e6
Mac station <guoliang.he@s-cube.com>
2020-12-31 15:22:01 +0000

    lazy_commie

--- hotel_cloud/gps.py
+++ hotel_cloud/gps.py
@@ -85,7 +85,7 @@ class gp_model:
 class SpectralMixtureGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super(SpectralMixtureGPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.LinearMean()
+        self.mean_module = gpytorch.means.LinearMean(input_size=1)
         self.covar_module = gpytorch.kernels.SpectralMixtureKernel(num_mixtures=4)
         self.covar_module.initialize_from_data(train_x, train_y)
 
