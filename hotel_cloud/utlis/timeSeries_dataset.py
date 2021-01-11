0bd074be9a61e536451da8743f25f35187bb6156
Mac station <guoliang.he@s-cube.com>
2021-01-11 16:20:33 +0000

    lazy_commit

--- hotel_cloud/utlis/timeSeries_dataset.py
+++ hotel_cloud/utlis/timeSeries_dataset.py
@@ -3,7 +3,6 @@ import pandas as pd
 from tslearn.clustering import TimeSeriesKMeans
 import datetime
 
-
 class timeSeries_data:
 
     def __init__(self, **kwargs):
@@ -13,20 +12,15 @@ class timeSeries_data:
                         df: pd.DataFrame, 
                         stay_date: str, 
                         preserved_col: list,  # list of feature name to preserve
-                        year = None,
                         ):
-        if year is None:
-            return df[(df["staydate"] == str(self.year) +"-"+ stay_date)].groupby("lead_in").sum().filter(preserved_col)
+        return df[(df["staydate"] == str(self.year) +"-"+ stay_date)].groupby("lead_in").sum().filter(preserved_col)
 
-        if isinstance(year, int):
-            year = str(year)
-        return df[(df["staydate"] == year +"-"+ stay_date)].groupby("lead_in").sum().filter(preserved_col)
 
-    def check_timeseries(self, history: int = 100, filter_zero = True):
+    def check_timeseries(self, df, history: int = 100, filter_zero = True):
 
         start_date = datetime.datetime(self.year, 1, 1, 0, 0)
         dates = [None]*365; dates[0] = start_date.strftime("%m-%d")
-        data=np.empty((365, history), dtype=np.float32)
+        data = np.empty((365, history), dtype=np.float32)
 
         for i in range(1, 365):
             start_date += datetime.timedelta(days=1)
@@ -36,7 +30,7 @@ class timeSeries_data:
             s_df = df[(df["staydate"] == str(self.year) +"-"+ dates[i])].groupby("lead_in").sum()
             d = s_df["rooms_all"].to_numpy()
 
-            if len(d) >=history:
+            if len(d) >= history:
                 data[i,:] = np.flip(d[:history])
             else:
                 data[i,:] = np.zeros((history, ))
