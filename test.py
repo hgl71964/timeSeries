e19420346e511acec0f1f4b710f514630988ac4f
Mac station <guoliang.he@s-cube.com>
2021-01-13 10:44:39 +0000

    lazy_commit

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-
 a = [i for i in range(10)]
 
 print(a)
 
 a.reverse()
 print(a)
+
+class abc:
+
+    def __init__(self, something):
+        return None
+
+    @staticmethod
+    def foo(self):
+        return False
+
