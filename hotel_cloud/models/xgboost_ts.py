fcb110dfac698d930bf85f7cdd0b51c8b84825ad
Mac station <guoliang.he@s-cube.com>
2021-01-15 11:57:07 +0000

    lazy_commit

--- hotel_cloud/models/xgboost_ts.py
+++ hotel_cloud/models/xgboost_ts.py
@@ -1,29 +1,36 @@
-import xgboost as xgb
+from xgboost import train
+from xgboost import Booster
 from pandas import DataFrame
 
 """
-dtrain = xgb.DMatrix(d, d["rooms_all"])
-dtest = xgb.DMatrix(test_df, test_df["rooms_all"])
-
-# specify parameters via map, definition are same as c++ version
-param = {'max_depth':6, 'eta':1, 'silent':1, 'objective':'reg:squarederror'}
-
-# specify validations set to watch performance
-watchlist = [(dtest, 'eval'), (dtrain, 'train')]
-num_round = 2
-bst = xgb.train(param, dtrain, num_round, watchlist)
+low level interface to XGboost
+model Booster; train via xgb.train
 """
 
-def train(train_df, 
-        test_df, 
+def xgb_train(train_df: DataFrame, 
+        test_df: DataFrame, 
         target: str, 
-        param):
+        param: dict,
+        n_estimators: int = 10,  # num_boost_round
+        ):
 
+    """
+    Return:
+        Booster
+    """
     if not isinstance(train_df, DataFrame):
         raise TypeError("must provide pf")
 
+    # make core data structure
     dtrain = xgb.DMatrix(train_df, train_df[target])
     dtest = xgb.DMatrix(test_df, test_df[target])
 
+    if "max_depth" not in param:
+        param.update({"max_depth":6})
+    if "eta" not in param:
+        param.update({"eta":6})
 
-    return None
+    watchlist = [(dtest, 'eval'), (dtrain, 'train')]
+
+    return xgb.train(param, dtrain, n_estimators, watchlist)
+
