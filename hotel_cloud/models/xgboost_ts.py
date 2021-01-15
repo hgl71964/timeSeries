e7546e80e2ae68c827bf63b822a87c845531c3c7
Mac station <guoliang.he@s-cube.com>
2021-01-15 12:03:50 +0000

    lazy_commit

--- hotel_cloud/models/xgboost_ts.py
+++ hotel_cloud/models/xgboost_ts.py
@@ -8,7 +8,7 @@ model Booster; train via xgb.train
 """
 
 def xgb_train(train_df: DataFrame, 
-        test_df: DataFrame, 
+        test_df: DataFrame = None, 
         target: str, 
         param: dict,
         n_estimators: int = 10,  # num_boost_round
@@ -23,17 +23,20 @@ def xgb_train(train_df: DataFrame,
 
     # make core data structure
     dtrain = xgb.DMatrix(train_df, train_df[target])
-    dtest = xgb.DMatrix(test_df, test_df[target])
 
+    if test_df is not None:
+        dtest = xgb.DMatrix(test_df, test_df[target])
+        watchlist = [(dtest, 'eval'), (dtrain, 'train')]
+    else:
+        watchlist = [(dtrain, 'train')]
+
+    # make params
     if "max_depth" not in param:
         param.update({"max_depth":6})
     if "eta" not in param:
         param.update({"eta":1e-1})
     if "silent" not in param:
         param.update({"silent":1})
-
-
-    watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 
     return xgb.train(param, dtrain, n_estimators, watchlist)
 
