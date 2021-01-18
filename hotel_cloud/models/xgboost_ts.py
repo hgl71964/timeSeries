e265b55c72b4cb6bf495ce47faaebc361e561e40
Guoliang HE <guolianghe1996@gmail.com>
2021-01-18 12:08:40 +0000

    lazy_commit

--- hotel_cloud/models/xgboost_ts.py
+++ hotel_cloud/models/xgboost_ts.py
@@ -4,6 +4,7 @@ from xgboost import DMatrix
 from pandas import DataFrame
 from xgboost import cv
 from sklearn.model_selection import KFold
+from typing import List
 
 
 """
@@ -39,52 +40,64 @@ def xgb_train(train_df: DataFrame,
     return train(param, dtrain, n_estimators, watchlist)
 
 
-def CV(full_df: DataFrame, 
+def xgb_cv(full_df: DataFrame, 
         data_dict: dict,  # index -> date
-        target: str, 
         param: dict,
-        n_estimators: int = 10,  # num_boost_round
-        nfold=3, 
-        make_folds: callable = lambda x: None,  # return list[list[str]]
-        make_lag_from_dates: callable = lambda x: None,  # make lag feature given list of dates
+        n_estimators: int,  # num_boost_round
+        nfold: int, 
+        ts: object,  # a timeSeries_data object
+        preserved_cols: List[str], 
+        target: str, 
+        history: int, 
+        lag_bound: tuple, 
         ):
-    
-    folds = make_folds(nfold)  # List[List[str]]
+    """
+    hand-made cross-validation
+    """
+    kf = KFold(n_splits=nfold, shuffle=False)
 
+    for train_keys, test_keys in kf.split(list(data_dict.keys())):  # CV
 
-    
+        train_dates, test_dates = [data_dict[j] for j in train_keys], \
+                                    [data_dict[i] for i in test_keys]
+        train_df = ts.make_lag_from_dates(full_df, train_dates, preserved_cols, \
+                                        target, history, lag_bound)
+        test_df = ts.make_lag_from_dates(full_df, test_dates, preserved_cols,\
+                                         target, history,lag_bound)
+
+        bst = xgb_train(train_df, test_df, target, param, n_estimators)
 
     return None
 
 
 
-def xgb_CV(full_df: DataFrame, 
-        target: str, 
-        param: dict,
-        n_estimators: int = 10,  # num_boost_round
-        nfold=3, 
-        metrics=(),
-        obj=None,
-        feval=None, 
-        early_stopping_rounds=None,
-        verbose_eval=None, 
-        callbacks=None,
-        ) -> Booster:
-
-    """
-    this does not preserve sequential property
-    """
-
-    if not isinstance(full_df, DataFrame):
-        raise TypeError("must provide pf")
-    
-    # make core data structure
-    feats = [i for i in full_df.columns if i != target]
-    dcv = DMatrix(full_df[feats], label=full_df[target])
-
-    # metric = eval_metric 'User can add multiple evaluation metrics' 
-    return cv(param, dcv, num_boost_round=n_estimators, nfold=nfold,
-                metrics=metrics, obj=obj, feval=feval, 
-                early_stopping_rounds=early_stopping_rounds,
-                verbose_eval=verbose_eval, callbacks=callbacks)
+# def xgb_CV(full_df: DataFrame, 
+#         target: str, 
+#         param: dict,
+#         n_estimators: int = 10,  # num_boost_round
+#         nfold=3, 
+#         metrics=(),
+#         obj=None,
+#         feval=None, 
+#         early_stopping_rounds=None,
+#         verbose_eval=None, 
+#         callbacks=None,
+#         ) -> Booster:
+# 
+#     """
+#     this does not preserve sequential property
+#     """
+# 
+#     if not isinstance(full_df, DataFrame):
+#         raise TypeError("must provide pf")
+#     
+#     # make core data structure
+#     feats = [i for i in full_df.columns if i != target]
+#     dcv = DMatrix(full_df[feats], label=full_df[target])
+# 
+#     # metric = eval_metric 'User can add multiple evaluation metrics' 
+#     return cv(param, dcv, num_boost_round=n_estimators, nfold=nfold,
+#                 metrics=metrics, obj=obj, feval=feval, 
+#                 early_stopping_rounds=early_stopping_rounds,
+#                 verbose_eval=verbose_eval, callbacks=callbacks)
     
