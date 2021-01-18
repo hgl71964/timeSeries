e8654ec7d870954436f73a86a89409c98ca71a53
Guoliang HE <guolianghe1996@gmail.com>
2021-01-18 18:13:19 +0000

    add KMeans model selection

--- hotel_cloud/models/kMeansTimeSeries.py
+++ hotel_cloud/models/kMeansTimeSeries.py
@@ -1,8 +1,18 @@
 from tslearn.clustering import TimeSeriesKMeans
+from tslearn.barycenters import softdtw_barycenter
+from tslearn.metrics import soft_dtw
+import matplotlib.pyplot as plt
 
-def ts_k_means(data, **kwargs):
-    n_cluster, epochs, metric = kwargs.get("n_cluster", 7), kwargs.get("epochs", 128), \
+def ts_k_means(data, n_cluster, **kwargs):
+    epochs, metric = kwargs.get("epochs", 128), \
                             kwargs.get("metric", "softdtw")
 
     km = TimeSeriesKMeans(n_clusters=n_cluster,max_iter = epochs, metric=metric)
     return km.fit_predict(data)  
+
+def k_mean_model_selection(data, n_possible_cluster: int = 7, **kwargs):
+
+    for num in range(2, n_possible_cluster):
+        labels = ts_k_means(data, num, **kwargs)
+
+    return None
