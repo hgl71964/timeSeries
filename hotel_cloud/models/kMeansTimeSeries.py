abc2b34a658553080769e515747145ca0a77835d
Mac station <guoliang.he@s-cube.com>
2021-01-19 11:07:24 +0000

    lazy_commit

--- hotel_cloud/models/kMeansTimeSeries.py
+++ hotel_cloud/models/kMeansTimeSeries.py
@@ -3,20 +3,28 @@ from tslearn.barycenters import softdtw_barycenter
 from tslearn.metrics import soft_dtw
 import matplotlib.pyplot as plt
 
-def ts_k_means(data, n_cluster, **kwargs):
+def Kmeans_predict(data, n_cluster, **kwargs):
     epochs, metric = kwargs.get("epochs", 128), \
                             kwargs.get("metric", "softdtw")
 
     km = TimeSeriesKMeans(n_clusters=n_cluster,max_iter = epochs, metric=metric)
-    return km.fit_predict(data)  
+    return km, km.fit_predict(data)  # model, labels
 
 def k_mean_model_selection(data, n_possible_cluster: int = 7, **kwargs):
 
-    for num in range(2, n_possible_cluster):
-        labels = ts_k_means(data, num, **kwargs)
+    losses = []
+    for num in range(2, n_possible_cluster+1):
+        km, _ = Kmeans_predict(data, num, **kwargs)
+        loss = km.inertia_
+        losses.append(loss)
 
-        # TODO add inertia...
+    indice = [i for i in range(2, n_possible_cluster+1)]
 
-        
+    _, ax = plt.subplots()
 
+    ax.scatter(indice, losses, label="loss function")
+    ax.set_xlabel("Num of clusters"); 
+    ax.set_ylabel("loss function"); 
+    # ax.set_title(f"time series plot")
+    plt.show()
     return None
