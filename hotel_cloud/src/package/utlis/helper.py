892ece2e23960fdc05d860c3931f91c54b20d6db
Mac station <guoliang.he@s-cube.com>
2021-02-04 11:45:18 +0000

    add scores

--- hotel_cloud/src/package/utlis/helper.py
+++ hotel_cloud/src/package/utlis/helper.py
@@ -8,57 +8,6 @@ import os
 class helper:
     
     @staticmethod
-    def _CV_average_over_folds(df):
-        df["max_of_maxes"] = df.max()["max"]
-        df["min_of_mines"] = df.min()["min"]
-        df["mean_of_means"] = df.mean()["mean"]
-        df = df.drop(columns=["min", "max", "mean"]).iloc[0]
-        return df
-    
-    @staticmethod
-    def _CV_add_weighted_mean(df, num_groups, metric_name):
-
-        size_recorder, mean_recorder = [], []        
-        temp_df = df[df["metric"]==f"{metric_name}"]
-        for i in range(num_groups):
-
-            size_recorder.append(   \
-                    float(temp_df[temp_df["group_label"]==i]["group_size"].to_numpy()))
-            mean_recorder.append(   \
-                    float(temp_df[temp_df["group_label"]==i]["mean_of_means"].to_numpy()))
-
-        size_recorder, mean_recorder = np.array(size_recorder), np.array(mean_recorder)
-        fraction = size_recorder/size_recorder.sum()
-        df[f"{metric_name}_weighted_mean"] = np.multiply(fraction, mean_recorder).sum()
-        return df
-
-    @staticmethod
-    def CV_post_process(num_groups: int,  # numbers of group in total
-                    *args):
-
-        """
-        make sure feature names are consistent (do not rename existing feature...)
-        """
-        softdtw_df, mse_df = [], []
-
-        for df in args:
-
-            # add stats over folds, -> pd.series
-            series = helper._CV_average_over_folds(df)
-
-            if series["metric"] == "softdtw":
-                softdtw_df.append(series)
-            elif series["metric"] == "mse":
-                mse_df.append(series)
-
-        if len(softdtw_df) == 1:  # on the unclustered data, i.e. group_num = -1
-            return pd.concat(softdtw_df+mse_df, axis=1).T
-        else:
-            df1 = helper._CV_add_weighted_mean(pd.concat(softdtw_df,axis=1).T, num_groups, "softdtw")
-            df2 = helper._CV_add_weighted_mean(pd.concat(mse_df,axis=1).T, num_groups, "mse")
-            return pd.concat([df1, df2], axis=0)
-
-    @staticmethod
     def feature_important(bst, name, cat_list):
         if name == "xgb":
             
